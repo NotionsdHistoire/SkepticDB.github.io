27d0c6817dcf773f061ca96f93856169be8f1c2e
Denis Migdal <denis@migdal.ovh>
2017-08-10 14:04:10 +0200

    JSON Validation

---
+++ js/dynpages.js
@@ -0,0 +1,45 @@
+function verifyJSON(jsonData) {
+	console.log(jsonData);
+	console.log("JSON verification :");	
+	let result = validate_obj(jsonData)
+	console.log(result);
+}
+
+function setHTML(htmlData, jsonData){
+	$("#content").html(htmlData);
+	init(jsonData);
+
+	$("v").each(function(id, elem){
+		elem = $(elem);
+		elem.html( jsonData[elem.html()] );
+		
+	});
+}
+
+function downloadHTML(name, callback) {
+	$.get(htmlURI(name), callback);
+}
+
+function downloadJSON(name, callback) {
+	$.get(jsonURI(name), callback);
+}
+
+function htmlURI(name){
+	return "html/" + name.toLowerCase() + ".html";
+}
+
+function jsonURI(name) {
+	return "json/" + name + ".json";
+}
+
+function getParams() {
+	let params = {"p": "index"};
+	let str_params = window.location.search.substr(1);
+	str_params = str_params.split("&");
+
+	str_params.forEach( function(elem){
+		param = elem.split("=");
+		params[param[0]] = param[1];
+	});
+	return params;
+}
---
+++ js/validator.js
@@ -0,0 +1,39 @@
+var ajv = new Ajv();
+
+function validate_str(str) {
+	return __validate(JSON.parse(obj));
+}
+
+function validate_obj(obj) {
+	return __validate(obj);
+}
+
+
+function __validate(obj) {
+
+	schemaURI = obj["@context"] + obj["@type"] + ".json";
+	var valid = ajv.validate(schemaURI, obj);
+	if (!valid)
+		return ajv.errors;
+	return true;
+}
+
+function preload_schemas(avj, context, types) {
+
+	types.forEach( function(type) {
+		url = context + type + ".json";
+
+		$.ajax({
+			url: url,
+			async: false,
+			success: function(schema) {
+				console.log("Schema : " + url);
+				avj.addSchema(schema, url);
+			}
+		});
+	});
+}
+
+context = "http://localhost/SkepticDB.github.io/json-schema/";
+types = ["entity", "member", "organization", "person"];
+preload_schemas(ajv,context, types);
